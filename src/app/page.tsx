32e326d0dc9fea31f5421e02758284b49496e3a3
yavuz dogan <y.selimdogan@gmail.com>
2024-03-19 11:36:55 +0300

    results are coming from api

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,41 @@
 "use client";
 
-import { fetchCharactersByName } from "@/backendInterface/apiQueries";
+import {
+  fetchCharactersByName,
+  Character,
+} from "@/backendInterface/apiQueries";
 import styles from "./page.module.css";
 import { useState } from "react";
+import Image from "next/image";
 
 export default function Home() {
   const [searchTerm, setSearchTerm] = useState("");
-  const [episodes, setEpisodes] = useState([]);
+  const [characters, setCharacters] = useState<Character[]>([]);
+  const [selectedCharacters, setSelectedCharacters] = useState({}); // {id: true}
 
-  const onInputChange = async(e) => {
+  const onInputChange = async (e) => {
     const term = e.target.value;
     setSearchTerm(term);
-    if(term.length > 2) {
-      const characters = await fetchCharactersByName(term);
-      console.log(characters);
+    if (term.trim().length > 2) {
+      const res = await fetchCharactersByName(term.trim());
+      if (res.results) {
+        setCharacters(res.results);
+      } else {
+        setCharacters([]);
+      }
     }
   };
+
+  const onCharacterChecked = (e) => {
+    const id = e.target.name;
+    let selectedChars = {...selectedCharacters};
+    if(e.target.checked){
+      selectedChars[id]  = true;
+    }
+    else delete selectedChars[id];
+    setSelectedCharacters(selectedChars);
+    console.log('selectedChars', selectedChars)
+  }
 
   const searcher = (
     <div>
@@ -24,11 +44,49 @@ export default function Home() {
         value={searchTerm}
         placeholder="Search"
         onChange={onInputChange}
+        className={styles.selectorInput}
       />
     </div>
   );
 
-  const resultList = <div>RESULT LIST</div>;
+  const resultList = (
+    <div className={styles.characterTable}>
+      {characters.map((c) => {
+        let trimmedSearchTerm = searchTerm.trim().toLocaleLowerCase();
+        let startIndexOfTheHighlight = c.name.toLowerCase().indexOf(trimmedSearchTerm);
+
+        let characterNameFirstPart = c.name.substring(0, startIndexOfTheHighlight);
+        let characterNameSecondPart = c.name.substring(startIndexOfTheHighlight, startIndexOfTheHighlight + trimmedSearchTerm.length);
+        let characterNameLastPart = c.name.substring(startIndexOfTheHighlight + trimmedSearchTerm.length, c.name.length);
+
+        return (
+          <div className={styles.characterRow} key={c.id}>
+            <input name={c.id.toString()} type="checkbox" className={styles.rowCheckBox} checked={selectedCharacters[c.id]?? false} onChange={onCharacterChecked}/>
+            <Image
+              src={c.image}
+              alt={c.name}
+              width={60}
+              height={60}
+              className={styles.characterImage}
+            />
+
+            <div className={styles.characterInfoContainer}>
+              <span className={styles.characterName}>
+                <span>{characterNameFirstPart}</span>
+                <strong >{characterNameSecondPart}</strong>
+                <span>{characterNameLastPart}</span>
+
+              </span>
+              <span className={styles.characterEpisode}>
+                {c.episode.length + " Episodes"}
+              </span>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+
 
   return (
     <main className={styles.main}>
