83152a57458d067de8a4ee03f08fe7a34f6dba2a
yavuz dogan <y.selimdogan@gmail.com>
2024-03-19 13:41:53 +0300

    functionally completed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ import Image from "next/image";
 export default function Home() {
   const [searchTerm, setSearchTerm] = useState("");
   const [characters, setCharacters] = useState<Character[]>([]);
-  const [selectedCharacters, setSelectedCharacters] = useState({}); // {id: true}
+  const [selectedCharacters, setSelectedCharacters] = useState({}); // {id: selected_character}
 
   const onInputChange = async (e) => {
     const term = e.target.value;
@@ -23,22 +23,38 @@ export default function Home() {
       } else {
         setCharacters([]);
       }
-    }
+    } else setCharacters([]);
   };
 
-  const onCharacterChecked = (e) => {
-    const id = e.target.name;
-    let selectedChars = {...selectedCharacters};
-    if(e.target.checked){
-      selectedChars[id]  = true;
-    }
-    else delete selectedChars[id];
+  const onCharacterChecked = (c: Character, isChecked: boolean) => {
+    let selectedChars = { ...selectedCharacters };
+    if (isChecked) {
+      selectedChars[c.id] = c;
+    } else delete selectedChars[c.id];
     setSelectedCharacters(selectedChars);
-    console.log('selectedChars', selectedChars)
-  }
+  };
 
   const searcher = (
-    <div>
+    <div className={styles.searchContainer}>
+      <div className={styles.selectedNames}>
+        {Object.keys(selectedCharacters).map((char_id) => {
+          return (
+            <span key={char_id} className={styles.selectedNameBox}>
+              {selectedCharacters[char_id].name}
+              <Image
+                alt="close"
+                src="/close_image.png"
+                width={25}
+                height={25}
+                style={{ margin: 5 }}
+                onClick={() => {
+                  onCharacterChecked(selectedCharacters[char_id], false);
+                }}
+              />
+            </span>
+          );
+        })}
+      </div>
       <input
         type="text"
         value={searchTerm}
@@ -53,15 +69,32 @@ export default function Home() {
     <div className={styles.characterTable}>
       {characters.map((c) => {
         let trimmedSearchTerm = searchTerm.trim().toLocaleLowerCase();
-        let startIndexOfTheHighlight = c.name.toLowerCase().indexOf(trimmedSearchTerm);
+        let startIndexOfTheHighlight = c.name
+          .toLowerCase()
+          .indexOf(trimmedSearchTerm);
 
-        let characterNameFirstPart = c.name.substring(0, startIndexOfTheHighlight);
-        let characterNameSecondPart = c.name.substring(startIndexOfTheHighlight, startIndexOfTheHighlight + trimmedSearchTerm.length);
-        let characterNameLastPart = c.name.substring(startIndexOfTheHighlight + trimmedSearchTerm.length, c.name.length);
+        let characterNameFirstPart = c.name.substring(
+          0,
+          startIndexOfTheHighlight
+        );
+        let characterNameSecondPart = c.name.substring(
+          startIndexOfTheHighlight,
+          startIndexOfTheHighlight + trimmedSearchTerm.length
+        );
+        let characterNameLastPart = c.name.substring(
+          startIndexOfTheHighlight + trimmedSearchTerm.length,
+          c.name.length
+        );
 
         return (
           <div className={styles.characterRow} key={c.id}>
-            <input name={c.id.toString()} type="checkbox" className={styles.rowCheckBox} checked={selectedCharacters[c.id]?? false} onChange={onCharacterChecked}/>
+            <input
+              name={c.id.toString()}
+              type="checkbox"
+              className={styles.rowCheckBox}
+              checked={selectedCharacters[c.id] ?? false}
+              onChange={(e) => onCharacterChecked(c, e.target.checked)}
+            />
             <Image
               src={c.image}
               alt={c.name}
@@ -73,9 +106,8 @@ export default function Home() {
             <div className={styles.characterInfoContainer}>
               <span className={styles.characterName}>
                 <span>{characterNameFirstPart}</span>
-                <strong >{characterNameSecondPart}</strong>
+                <strong>{characterNameSecondPart}</strong>
                 <span>{characterNameLastPart}</span>
-
               </span>
               <span className={styles.characterEpisode}>
                 {c.episode.length + " Episodes"}
@@ -86,7 +118,6 @@ export default function Home() {
       })}
     </div>
   );
-
 
   return (
     <main className={styles.main}>
